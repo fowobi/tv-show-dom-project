dbb45dad24ad235680190b233cadd79c303af757
fowobi <109865505+fowobi@users.noreply.github.com>
2023-05-01 23:00:32 +0100

    Updated JavaScript

--- script.js
+++ script.js
@@ -1,12 +1,103 @@
-//You can edit ALL of the code here
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
+  addSearchFunctionality(allEpisodes);
+  addSelectFunctionality(allEpisodes);
 }
 
+   //This is Displaying Number Code 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  rootElem.innerHTML = "";
+  const episodeCountElem = document.createElement("pi");
+  episodeCountElem.textContent = `Displaying ${episodeList.length} / ${getAllEpisodes().length} episodes`;
+  rootElem.appendChild(episodeCountElem);
+
+  //This big container for the Movie(grid-container)
+  const gridContainer = document.createElement("div");
+  gridContainer.className = "grid-container";
+  rootElem.appendChild(gridContainer);
+
+  //This is the each movie Card Code(grid-item)
+  episodeList.forEach((episode) => {
+    const gridItem = document.createElement("div");
+    gridItem.className = "grid-item";
+    gridContainer.appendChild(gridItem);
+
+   //This is Movie Title Code both episode-Name and episode-Code
+    const episodeCode = getEpisodeCode(episode);
+    const episodeTitle = `${episode.name} - ${episodeCode} `;
+    const episodeTitleElem = document.createElement("h2");
+    episodeTitleElem.textContent = episodeTitle;
+    gridItem.appendChild(episodeTitleElem);
+   
+    //This is Image Code
+    const imgElem = document.createElement("img");
+    imgElem.src = episode.image.medium;
+    imgElem.alt = episodeTitle;
+    gridItem.appendChild(imgElem);
+
+
+   // This is the Episode summary code 
+    const summaryElem = document.createElement("summary");
+    summaryElem.innerHTML = episode.summary;
+    gridItem.appendChild(summaryElem);
+  });
+}
+
+//This is the Season Code
+function getEpisodeCode(episode) {
+  const season = episode.season.toString().padStart(2, "0");
+  const number = episode.number.toString().padStart(2, "0");
+  return `S${season}E${number}`;
+}
+
+//This is Search input Code
+function addSearchFunctionality(allEpisodes) {
+  const searchElem = document.createElement("input");
+  searchElem.type = "text";
+  searchElem.placeholder = "Search episodes";
+  searchElem.addEventListener("input", () => {
+    const searchTerm = searchElem.value.trim().toLowerCase();
+    const filteredEpisodes = allEpisodes.filter((episode) => {
+      const name = episode.name.toLowerCase();
+      const summary = episode.summary.toLowerCase();
+      return name.includes(searchTerm) || summary.includes(searchTerm);
+    });
+    
+    makePageForEpisodes(filteredEpisodes);
+     updateEpisodeCount(filteredEpisodes.length, allEpisodes.length);
+  });
+
+  const rootElem = document.getElementById("root");
+  rootElem.insertBefore(searchElem, rootElem.firstChild);
+}
+
+//This is Select Code
+
+function addSelectFunctionality(allEpisodes) {
+  const selectElem = document.createElement("select");
+  selectElem.addEventListener("change", () => {
+    const selectedOption = selectElem.options[selectElem.selectedIndex];
+    const selectedEpisode = allEpisodes.find(
+      (episode) => getEpisodeCode(episode) === selectedOption.value
+    );
+    makePageForEpisodes([selectedEpisode]);
+  });
+
+  allEpisodes.forEach((episode) => {
+    const episodeOption = document.createElement("option");
+    episodeOption.value = getEpisodeCode(episode);
+    episodeOption.textContent = getEpisodeCode(episode) + " - " + episode.name;
+    selectElem.appendChild(episodeOption);
+  });
+
+  const selectWrapperElem = document.createElement("div");
+  selectWrapperElem.appendChild(selectElem);
+
+  const rootElem = document.getElementById("root");
+  rootElem.insertBefore(selectWrapperElem, rootElem.firstChild);
 }
 
 window.onload = setup;
